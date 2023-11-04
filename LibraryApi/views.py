1ea3eabe7099e4919d5ee3060e8ae804d8d0e4b9
Yaqwb Naqeb <Yaqub-naqeb>
2023-11-04 15:56:30 +0300

    last version

--- LibraryApi/views.py
+++ LibraryApi/views.py
@@ -1,45 +1,47 @@
-from django.shortcuts import render,HttpResponse
-from rest_framework.response import Response
-from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
-from django_filters.rest_framework import DjangoFilterBackend
-from .filters import BookFilter
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework.authentication import TokenAuthentication, SessionAuthentication
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.pagination import PageNumberPagination
+# from django.shortcuts import render,HttpResponse
+# from rest_framework.response import Response
+# from rest_framework import viewsets
+# from .models import Book
+# from .serializers import BookSerializer
+# from django_filters.rest_framework import DjangoFilterBackend
+# from .filters import BookFilter
+# from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+# from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+# from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPagination(PageNumberPagination):
-    page_size = 4
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+# class CustomPagination(PageNumberPagination):
+#     page_size = 3
+#     page_size_query_param = 'page_size'
+#     max_page_size = 100
 
 
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = BookFilter
-    authentication_classes = [JWTAuthentication, TokenAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    pagination_class = CustomPagination 
-    def list(self, request, *args, **kwargs):
-        # Get the queryset of all books
-        queryset = self.filter_queryset(self.get_queryset())
+# class BookViewSet(viewsets.ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_class = BookFilter
+#     authentication_classes = [JWTAuthentication, TokenAuthentication, SessionAuthentication]
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#     pagination_class = CustomPagination 
+    
+    
+#     def list(self, request, *args, **kwargs):
+#         # Get the queryset of all books
+#         queryset = self.filter_queryset(self.get_queryset())
         
-        # Calculate the count of all books
-        book_count = queryset.count()
+#         # Calculate the count of all books
+#         book_count = queryset.count()
 
-        # Serialize the data and include the count
-        serializer = BookSerializer(queryset, many=True)
-        data = {
-            'book_count': book_count,
-            'books': serializer.data
-        }
-        return Response(data)
+#         # Serialize the data and include the count
+#         serializer = BookSerializer(queryset, many=True)
+#         data = {
+#             'book_count': book_count,
+#             'books': serializer.data
+#         }
+#         return Response(data)
     
     
     
@@ -66,3 +68,47 @@ class BookViewSet(viewsets.ModelViewSet):
 
 # class JustForTest(viewsets.ModelViewSet):
     
+    
+    
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import BookFilter
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+
+class CustomPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
+    authentication_classes = [JWTAuthentication, TokenAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination 
+
+    @action(detail=False)
+    def count(self, request):
+        book_count = self.get_queryset().count()
+        return Response({'book_count': book_count})
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Apply pagination
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = BookSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
