482837d4bbee32458e8e46d03581013fb3bcf5d0
Yaqwb Naqeb <Yaqub-naqeb>
2023-11-06 01:31:32 +0300

    category name

--- LibraryApi/serializers.py
+++ LibraryApi/serializers.py
@@ -2,10 +2,17 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    category_name = serializers.SerializerMethodField()
     class Meta:
         model = Book
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('id', 'title', 'author', 'genre', 'publication_date', 'is_booked', 'cover_image', 'category_name')
+        
+    def get_category_name(self, obj):
+        return obj.category.name if obj.category else None
 
 class BooksWithCountSerializer(serializers.Serializer):
     book_count = serializers.IntegerField()
     books = BookSerializer(many=True)
+    
+
