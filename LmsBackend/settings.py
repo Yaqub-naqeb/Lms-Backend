341f738dc0aedc02c636ca95a197b7cc8adc61fa
NergizOmer <98105569+NergizOmer@users.noreply.github.com>
2024-03-06 22:13:54 +0300

    Update settings.py

--- LmsBackend/settings.py
+++ LmsBackend/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'NAME': 'LMSDB',
         'HOST': 'localhost',
         'USER': 'root',  # Your MySQL username
-        'PASSWORD': '12WWdf$$k',  # Your MySQL password
+        'PASSWORD': '12345678',  # Your MySQL password
         'PORT': '3306',  # MySQL default port
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
